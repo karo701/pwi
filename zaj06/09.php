838f9f1850114366f2823e48f7eb36ad22c52822
Karolina Szymańska <36896703+karo701@users.noreply.github.com>
2019-04-25 14:33:37 +0200

    Add files via upload

---
+++ zaj06/09.php
@@ -0,0 +1,5 @@
+<form action = "10.php" method = "post">
+  <input type = "number" name = "liczba1"><br>
+  <input type = "number" name = "liczba2"><br>
+  <input type = "submit" value = "Oblicz">
+</form>
